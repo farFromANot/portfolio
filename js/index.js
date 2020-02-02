680ba01ad06f7925a599aea451c9f12d5eb90990
farFromANot <iangibsonc@gmail.com>
2020-02-02 13:47:54 -0600

    move navbar to bottom of page, make it fixed to the bottom of page as well. Set up jumbotron and introduce yourself, as well as create a javascript file that will allow you to loop through and array of words and return it in the dom

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let myArr = new Array("Full Stack Developer", "Beat Maker", "Creative", "Visionary", "Student", "Basketball Player", "Vinyl Collector", "Aminmal Person");
+    loop();
+
+    function loop() 
+    {
+        $("#mySpan").slideUp(2000, function(){
+            myArr.push(myArr.shift());
+
+            $("#mySpan").empty();
+            $("#mySpan").append(myArr[0]);
+            $("#mySpan").show(300);
+
+            loop();
+        });
+    }
