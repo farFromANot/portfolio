443c58f4be4491fdb0da20eb9f7077fd47cc7875
Gibson <IanGibson@clearchannel.com>
2020-02-04 10:59:36 -0600

    add animation to navbar, it will show when viewers scroll

--- js/index.js
+++ js/index.js
@@ -1,15 +1,37 @@
-let myArr = new Array("Full Stack Developer", "Beat Maker", "Creative", "Visionary", "Student", "Basketball Player", "Vinyl Collector");
-    loop();
+(function ($) {
+    $(document).ready(function(){
+      
+      // hide .navbar first
+      $(".navbar").hide();
+      
+      // fade in .navbar
+      $(function () {
+          $(window).scroll(function () {
+              // set distance user needs to scroll before we fadeIn navbar
+              if ($(this).scrollTop() > 100) {
+                  $('.navbar').fadeIn();
+              } else {
+                  $('.navbar').fadeOut();
+              }
+          });
+  
+      
+      });
+      let myArr = new Array("Full Stack Developer", "Beat Maker", "Creative", "Visionary", "Student", "Basketball Player", "Vinyl Collector");
+        loop();
 
-    function loop() 
-    {
-        $("#mySpan").slideUp(2000, function(){
-            myArr.push(myArr.shift());
+        function loop() 
+        {
+            $("#mySpan").slideUp(2000, function(){
+                myArr.push(myArr.shift());
 
-            $("#mySpan").empty();
-            $("#mySpan").append(myArr[0]);
-            $("#mySpan").show(300);
+                $("#mySpan").empty();
+                $("#mySpan").append(myArr[0]);
+                $("#mySpan").show(300);
 
-            loop();
-        });
-    }
+                loop();
+            });
+        }
+  });
+    }(jQuery));
+
